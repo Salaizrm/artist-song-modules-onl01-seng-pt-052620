a5078259e9eda626778df482e29a8acdd61cac39
Robert Salaiz <work.salaizrm@gmail.com>
2020-07-03 05:04:22 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,17 +1,16 @@
 module Memorable
-  module ClassMethods
-    def reset_all
-      self.all.clear
-    end
+    module ClassMethods
+      def reset_all
+        self.all.clear
+      end
 
-    def count
-      self.all.count
-    end
-  end
-
-  module InstanceMethods
-    def initialize
-      self.class.all << self
+      def count
+        self.all.count
+      end
+    module InstanceMethods
+      def initialize
+        self.class.all << self
+      end
     end
   end
 end
